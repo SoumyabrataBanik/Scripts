dd4a2096683907869dcd17cb5975590b6a7db1e8
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2024-06-28 23:25:34 +0530

    Changes made

--- connect-bluetooth.sh
+++ connect-bluetooth.sh
@@ -9,6 +9,12 @@ echo ""
 echo $line
 echo ""
 
+status=$(rfkill list bluetooth | grep -i "Soft blocked" | awk '{print $3}')
+
+if [[ $status == "yes" ]]; then
+    rfkill unblock bluetooth
+fi
+
 sleep 2s
 
 bluetoothctl agent on
